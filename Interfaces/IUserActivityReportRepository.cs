afc6072f3c23a013a09f82ac11ed1470d26a02e3
Bhavik Goyal <bhavik.goyal@envasetechnologies.com>
2026-08-13 20:00:05 +0530

    changes team report

---
+++ Interfaces/IUserActivityReportRepository.cs
@@ -0,0 +1,12 @@
+﻿using AvecADeskApi.Model.UserActivity;
+
+namespace AvecADeskApi.Interfaces
+{
+    public interface IUserActivityReportRepository
+    {
+        Task<List<UserActivityResponse>> GetWorkReportAsync(
+            DateTime fromDate,
+            DateTime toDate,
+            string? employeeName = null);
+    }
+}
